01bdd912daa5b7edbe3d20ec1964ee1898b6b46d
Frederick Henderson <frederickjh@henderson-meier.org>
2015-09-25 10:21:44 +0200

    Removed development module enable on live site

--- localdevmeos.aliases.drushrc.php
+++ localdevmeos.aliases.drushrc.php
@@ -63,13 +63,6 @@ $aliases['live'] = array(
       'confirm-sanitizations' => TRUE,
       'no-ordered-dump' => TRUE,
       'no-cache' => TRUE,
-      'enable' => array(
-        'devel',
-        'stage_file_proxy',
-        'ds_ui',
-        'fields_ui',
-        'views_ui',
-      ),
     ),
   ),
 );
