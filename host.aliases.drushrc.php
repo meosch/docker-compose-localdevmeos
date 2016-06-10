ccea031bcfcc7c69b99dfc3e3c461cd9d1b1e350
Frederick Henderson <frederickjh@henderson-meier.org>
2016-06-10 17:42:54 +0200

    Updated the host aliases file template.

--- host.aliases.drushrc.php
+++ host.aliases.drushrc.php
@@ -1,15 +1,12 @@
 <?php
 $aliases['dev'] = array(
-  'root' => '/var/www',
-  'remote-user' => 'root',
-  'remote-host' => 'localhost',
-  'ssh-options' => '-p 9022', // Or any other port you specify when running the container
+  'parent' => '@docker.localdev',
   'path-aliases' => array(
     '%files' => 'sites/default/files', 
     ),
   'target-command-specific' => array(
     'sql-sync' => array(
-      'sanitize' => TRUE,
+//      'sanitize' => TRUE,
       'confirm-sanitizations' => TRUE,
       'no-ordered-dump' => TRUE,
       'no-cache' => TRUE,
@@ -32,6 +29,13 @@ $aliases['test'] = array(
     '%files' => 'sites/default/files',
     '%dump-dir' => 'sql-sync-tmp  ', 
     ),
+  'source-command-specific' => array(
+  'sql-sync'  => array(
+    'http-sync'  => 'http://example.com/dbouthouse/example-database-dump.sql.gz',
+    'http-sync-user' => 'username',
+    'http-sync-password' => 'password',
+    ),
+  ),
   'target-command-specific' => array(
     'sql-sync' => array(
       'sanitize' => TRUE,
@@ -66,3 +70,16 @@ $aliases['live'] = array(
     ),
   ),
 );
+// TLDR; MAGIC STARTS HERE.
+//
+// Remove "remote-host" from entries that correspond with the current server.
+// This allows us to use the same alias file in all environments.
+$ip = gethostbyname(php_uname('n'));
+foreach ($aliases as &$alias) {
+  if (empty($alias['remote-host'])) {
+    continue;
+  }
+  if (gethostbyname($alias['remote-host']) == $ip) {
+    unset($alias['remote-host']);
+  }
+}
