7793aac94e702b1b9fa0fba3ca075633b32761d6
Frederick Henderson <frederickjh@henderson-meier.org>
2016-06-10 14:20:39 +0200

    The previous change half-way worked. But inside all containers @self was resolving to @meosd7site. Not quite right.

--- host.aliases.drushrc.php
+++ host.aliases.drushrc.php
@@ -1,13 +1,15 @@
 <?php
 $aliases['dev'] = array(
   'root' => '/var/www',
-  'uri' => 'localdevmeos.docker',
+  'remote-user' => 'root',
+  'remote-host' => 'localhost',
+  'ssh-options' => '-p 9022', // Or any other port you specify when running the container
   'path-aliases' => array(
     '%files' => 'sites/default/files', 
     ),
   'target-command-specific' => array(
     'sql-sync' => array(
-//      'sanitize' => TRUE,
+      'sanitize' => TRUE,
       'confirm-sanitizations' => TRUE,
       'no-ordered-dump' => TRUE,
       'no-cache' => TRUE,
@@ -30,20 +32,12 @@ $aliases['test'] = array(
     '%files' => 'sites/default/files',
     '%dump-dir' => 'sql-sync-tmp  ', 
     ),
-  'source-command-specific' => array(
-  'sql-sync'  => array(
-    'http-sync'  => 'http://example.com/dbouthouse/example-database-dump.sql.gz',
-    'http-sync-user' => 'username',
-    'http-sync-password' => 'password',
-    ),
-  ),
   'target-command-specific' => array(
     'sql-sync' => array(
       'sanitize' => TRUE,
-//      'confirm-sanitizations' => TRUE,
+      'confirm-sanitizations' => TRUE,
       'no-ordered-dump' => TRUE,
       'no-cache' => TRUE,
-      'target-dump' => '/home/username/public_html/dbouthouse/example-database-dump.sql.gz',
       'enable' => array(
         'devel',
         'stage_file_proxy',
@@ -61,25 +55,14 @@ $aliases['live'] = array(
   'ssh-options' => '',    // port number ie. -p 22
   'path-aliases' => array(
     '%files' => 'sites/default/files',
-    '%dump-dir' => 'sql-sync-tmp', 
+    '%dump-dir' => 'sql-sync-tmp  ', 
     ),
   'target-command-specific' => array(
     'sql-sync' => array(
+      'sanitize' => TRUE,
+      'confirm-sanitizations' => TRUE,
       'no-ordered-dump' => TRUE,
       'no-cache' => TRUE,
     ),
   ),
 );
-// TLDR; MAGIC STARTS HERE.
-//
-// Remove "remote-host" from entries that correspond with the current server.
-// This allows us to use the same alias file in all environments.
-$ip = gethostbyname(php_uname('n'));
-foreach ($aliases as &$alias) {
-  if (empty($alias['remote-host'])) {
-    continue;
-  }
-  if (gethostbyname($alias['remote-host']) == $ip) {
-    unset($alias['remote-host']);
-  }
-}
